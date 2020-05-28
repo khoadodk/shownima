105544b11a72e55186b6c2af2d1f49cd082d36a2
Khoa Do <khoado.dk@gmail.com>
2020-05-28 11:17:02 -0400

    Added slideshow component

---
+++ src/components/main/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Main.scss';
+import MainContent from '../content/mainContent/MainContent';
+
+const Main = () => {
+  return (
+    <div className="main">
+      <MainContent />
+    </div>
+  );
+};
+
+export default Main;
