6e687a96825bacf98051487192f0cd50fb0f3409
Khoa Do <khoado.dk@gmail.com>
2020-06-02 16:37:28 -0400

    Added search movies functionality

--- src/redux/types.js
+++ src/redux/types.js
@@ -3,3 +3,5 @@ export const MOVIE_PAGE = 'MOVIE_PAGE';
 export const SET_ERROR = 'SET_ERROR';
 export const LOAD_MORE = 'LOAD_MORE';
 export const MOVIE_TYPE = 'MOVIE_TYPE';
+export const SEARCH_QUERY = 'SEARCH_QUERY';
+export const SEARCH_RESULT = 'SEARCH_RESULT';
